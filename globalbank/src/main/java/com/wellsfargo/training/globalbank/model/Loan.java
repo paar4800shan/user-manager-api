42147e8343e41f076b4b2f65d0648a76f01cec7b
kandibanda gopinath <88190872+gopi1611@users.noreply.github.com>
2022-11-06 23:47:26 +0530

    Update Loan.java

--- globalbank/src/main/java/com/wellsfargo/training/globalbank/model/Loan.java
+++ globalbank/src/main/java/com/wellsfargo/training/globalbank/model/Loan.java
@@ -1,5 +1,41 @@
 package com.wellsfargo.training.globalbank.model;
 
+mport java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Base64;
+
+//import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+/*
+The @Entity annotation specifies that the class is an entity and is mapped to a database table. 
+The @Table annotation specifies the name of the database table to be used for mapping. 
+*/
+@Entity
+@Table(name="Loan")
+
 public class Loan {
 
+  @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Loan(name="id")
+    private Long id;
+
+   @Loan(name="Customer id")
+    private String Customer id;
+
+   @Loan(name="Branch")
+    private String Branch;
+
+    @Loan(name="Loan Amount")
+    private String Loan Amount;
+  
 }
